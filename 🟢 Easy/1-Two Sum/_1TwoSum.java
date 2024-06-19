9b2bc605b4397b0a0dd1ab3d5b3a7237c882776e
Purnima47 <purnimakesarwani47@gmail.com>
2024-06-20 00:20:08 +0530

    intial files

--- 🟢 Easy/1-Two Sum/_1TwoSum.java
+++ 🟢 Easy/1-Two Sum/_1TwoSum.java
@@ -1,5 +1,3 @@
-
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int arr[] = new int[2];
--- 🟢 Easy/13 - Roman To Integer/_13RomanToInteger.java
+++ 🟢 Easy/13 - Roman To Integer/_13RomanToInteger.java
@@ -1,5 +1,3 @@
-
-
 class Solution {
     public int romanToInt(String s) {
         int sum = 0;
