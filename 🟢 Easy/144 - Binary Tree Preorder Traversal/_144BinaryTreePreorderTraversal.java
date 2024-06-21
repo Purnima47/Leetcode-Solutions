c861d68b6101a85bf7bf501b4fe0200821bc9c27
Purnima47 <purnimakesarwani47@gmail.com>
2024-06-22 02:18:37 +0530

    Binary tree

---
+++ 🟢 Easy/144 - Binary Tree Preorder Traversal/_144BinaryTreePreorderTraversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void preOrder(TreeNode root, ArrayList<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preOrder(root.left, res);
+        
+        preOrder(root.right, res);
+    }
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res=new ArrayList<>();
+        preOrder(root, res);
+        return res;
+    }
+}
