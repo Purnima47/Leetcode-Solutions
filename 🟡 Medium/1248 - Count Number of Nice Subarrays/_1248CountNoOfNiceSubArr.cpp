e27c494c5996a736cb8a6323c601773642dd6b4f
Purnima47 <purnimakesarwani47@gmail.com>
2024-06-23 03:18:58 +0530

    daily question 22June24

---
+++ 🟡 Medium/1248 - Count Number of Nice Subarrays/_1248CountNoOfNiceSubArr.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lessThanEqualTok(vector<int>& nums, int k) {
+        int sum = 0, count = 0, l = 0, r = 0;
+        while (r < nums.size()) {
+            sum += nums[r] % 2;
+            while (sum > k) {
+                sum -= nums[l] % 2;
+                l++;
+            }
+            count += r - l + 1;
+            r++;
+        }
+
+        return count;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return lessThanEqualTok(nums, k) - lessThanEqualTok(nums, k - 1);
+    }
+};
