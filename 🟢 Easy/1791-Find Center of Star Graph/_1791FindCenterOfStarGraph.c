6cd5e2e1312e91565ddcc337ccf9add3c1dd9fbc
Purnima47 <purnimakesarwani47@gmail.com>
2024-06-27 12:15:49 +0530

    daily question 27June24

---
+++ 🟢 Easy/1791-Find Center of Star Graph/_1791FindCenterOfStarGraph.c
@@ -0,0 +1,11 @@
+int findCenter(int **edges, int edgesSize, int *edgesColSize)
+{
+    if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+    {
+        return edges[0][0];
+    }
+    else
+    {
+        return edges[0][1];
+    }
+}
