f9cee5651f8dcaa1d2d624df57e7eff818c53c12
Purnima47 <purnimakesarwani47@gmail.com>
2024-06-30 23:31:08 +0530

    daily question 29June24

---
+++ 🟡 Medium/2192 - All Ancestors of a Node in a Directed Acylic Graph/_2192AllAncesOfANodeInADirectedAcyclicGraph.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void dfs(int node, List<List<Integer>> adjList, Set<Integer> visitedSet) {
+        visitedSet.add(node);
+
+        for (int dest : adjList.get(node)) {
+            if (!visitedSet.contains(dest)) {
+                dfs(dest, adjList, visitedSet);
+            }
+        }
+
+    }
+
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+
+        List<List<Integer>> adjList = new ArrayList<>();
+        List<List<Integer>> ancesList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int source = edge[0];
+            int destination = edge[1];
+
+            adjList.get(destination).add(source);
+        }
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> ancestors = new ArrayList<>();
+            Set<Integer> visitedSet = new HashSet<>();
+            dfs(i, adjList, visitedSet);
+
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (visitedSet.contains(j)) {
+                    ancestors.add(j);
+                }
+            }
+
+            ancesList.add(ancestors);
+        }
+        return ancesList;
+    }
+}
