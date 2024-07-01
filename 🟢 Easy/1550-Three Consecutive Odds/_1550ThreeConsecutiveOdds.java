821b44709059bb27275d9506beefb6bb7b3fcc89
Purnima47 <purnimakesarwani47@gmail.com>
2024-07-01 23:38:00 +0530

    daily question 1July24

---
+++ 🟢 Easy/1550-Three Consecutive Odds/_1550ThreeConsecutiveOdds.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        if (n == 1 || n == 2) {
+            return false;
+        }
+        int i = 0;
+        int j = 1;
+        int k = 2;
+        if (arr[i] % 2 != 0 && arr[j] % 2 != 0 && arr[k] % 2 != 0) {
+            return true;
+        }
+        while (!(arr[i] % 2 != 0 && arr[j] % 2 != 0 && arr[k] % 2 != 0) && (i < n - 3) && (j < n - 2) && (k < n - 1)) {
+            i++;
+            j++;
+            k++;
+            if (arr[i] % 2 != 0 && arr[j] % 2 != 0 && arr[k] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
