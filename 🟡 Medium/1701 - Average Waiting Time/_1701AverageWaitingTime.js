1ee23b21e7a7bddf3f37d17220c24b9575c806ff
Purnima47 <purnimakesarwani47@gmail.com>
2024-07-15 21:37:10 +0530

    daily question 9July24

---
+++ 🟡 Medium/1701 - Average Waiting Time/_1701AverageWaitingTime.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} customers
+ * @return {number}
+ */
+
+var averageWaitingTime = function (customers) {
+    let ans = 0;
+    let n = customers.length;
+    let arrivalTime = 0;
+
+    for (let i = 0; i < n; i++) {
+        arrivalTime = Math.max(customers[i][0], arrivalTime) + customers[i][1];
+        ans += arrivalTime - customers[i][0];
+    }
+
+    return ans / n;
+};
