88b0c446da33311a56a4fb35427d58c55e55719b
Purnima47 <purnimakesarwani47@gmail.com>
2024-07-15 21:36:09 +0530

    daily question 9July24

---
+++ 🟡 Medium/1701 - Average Waiting Time/_1701AverageWaitingTime.c
@@ -0,0 +1,20 @@
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+
+    return b;
+}
+
+double averageWaitingTime(int** customers, int customersSize,
+                          int* customersColSize) {
+    double ans = 0;
+    int arrivalTime = 0;
+
+    for (int i = 0; i < customersSize; i++) {
+        arrivalTime = max(customers[i][0], arrivalTime) + customers[i][1];
+        ans += arrivalTime - customers[i][0];
+    }
+
+    return ans / customersSize;
+}
