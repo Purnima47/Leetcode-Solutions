33475b8c6eb067ab5916240142acde6d616a8058
Purnima47 <purnimakesarwani47@gmail.com>
2024-07-27 11:06:24 +0530

    daily question 27July24

---
+++ 🟡 Medium/2976 - Minimum Cost to Covert String I/_2976MinCostToConvertStringI.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    void flyodWarshall(int n, vector<vector<long long>>& minCost) {
+
+        for (int i = 0; i < n; i++) {
+            minCost[i][i] = 0;
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    minCost[i][j] =
+                        min(minCost[i][j], minCost[i][k] + minCost[k][j]);
+                }
+            }
+        }
+    }
+
+    long long minimumCost(string source, string target, vector<char>& original,
+                          vector<char>& changed, vector<int>& cost) {
+        long long ans = 0;
+        int n = 26;
+        vector<vector<long long>> minCost(n, vector<long long>(n, INT_MAX));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                minCost[i][j] = INT_MAX;
+            }
+        }
+
+        for (int i = 0; i < original.size(); i++) {
+            int originalCharIdx = original[i] - 'a';
+            int changedCharIdx = changed[i] - 'a';
+            minCost[originalCharIdx][changedCharIdx] = min(
+                minCost[originalCharIdx][changedCharIdx], (long long)cost[i]);
+        }
+
+        flyodWarshall(n, minCost);
+
+        for (int i = 0; i < source.length(); i++) {
+            if (source.at(i) == target.at(i)) {
+                continue;
+            }
+
+            int sourceCharIdx = source.at(i) - 'a';
+            int targetCharIdx = target.at(i) - 'a';
+
+            if (minCost[sourceCharIdx][targetCharIdx] >= INT_MAX) {
+                return -1;
+            }
+
+            ans += minCost[sourceCharIdx][targetCharIdx];
+        }
+
+        return ans;
+    }
+};
