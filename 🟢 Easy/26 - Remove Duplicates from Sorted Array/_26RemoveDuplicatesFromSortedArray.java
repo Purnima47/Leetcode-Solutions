6b2a803720bf38e7f57c1d3ffb37dbe00ddecf93
Purnima47 <purnimakesarwani47@gmail.com>
2024-07-29 01:29:00 +0530

    ARRAY

--- 🟢 Easy/26 - Remove Duplicates from Sorted Array/_26RemoveDuplicatesFromSortedArray.java
+++ 🟢 Easy/26 - Remove Duplicates from Sorted Array/_26RemoveDuplicatesFromSortedArray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 0;
+        int i = 0; 
         for (int j = 1; j < nums.length; j++) {
             if (nums[i] != nums[j]) {
                 nums[i + 1] = nums[j];
