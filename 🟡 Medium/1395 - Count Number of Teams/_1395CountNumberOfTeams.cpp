1b2da5cf435931a91d5353523ccfafb0a5d50f0f
Purnima47 <purnimakesarwani47@gmail.com>
2024-07-30 00:25:37 +0530

    daily question 29July24

---
+++ 🟡 Medium/1395 - Count Number of Teams/_1395CountNumberOfTeams.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int teams = 0;
+        int n = rating.size();
+        vector<int> left(n);
+        vector<int> right(n);
+
+        for (int i = 1; i < n; i++) {
+            int lcount = 0;
+            int rcount = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (rating[i] > rating[j]) {
+                    lcount++;
+                } else {
+                    rcount++;
+                }
+            }
+
+            left[i] = lcount;
+            right[i] = rcount;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (rating[i] > rating[j]) {
+                    teams += left[j];
+                } else {
+                    teams += right[j];
+                }
+            }
+        }
+
+        return teams;
+    }
+};
---
+++ 🟡 Medium/1395 - Count Number of Teams/_1395CountNumberOfTeams.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numTeams(int[] rating) {
+        int teams = 0;
+        int n = rating.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            int lcount = 0;
+            int rcount = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (rating[i] > rating[j]) {
+                    lcount++;
+                } else {
+                    rcount++;
+                }
+            }
+
+            left[i] = lcount;
+            right[i] = rcount;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (rating[i] > rating[j]) {
+                    teams += left[j];
+                } else {
+                    teams += right[j];
+                }
+            }
+        }
+
+        return teams;
+    }
+}
