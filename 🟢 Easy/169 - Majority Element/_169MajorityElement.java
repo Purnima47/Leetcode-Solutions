e11ba7a6c9f98c80deaec99bb2649073edf2381e
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-09-12 18:10:27 +0530

    Array, Hash Table, Divide and Conquer, Sorting, Counting

---
+++ 🟢 Easy/169 - Majority Element/_169MajorityElement.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        return nums[n / 2];
+    }
+}
---
+++ 🟢 Easy/169 - Majority Element/_169MajorityElement.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let n = nums.length;
+    nums.sort((a, b) => a - b);
+
+    return nums[Math.floor(n / 2)];
+};
