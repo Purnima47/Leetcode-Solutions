97318bb5019a9808573142358c4c34f83b520c97
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-09-14 16:12:04 +0530

    Math, Recursion

---
+++ 🟢 Easy/326 - Power of Three/_326PowerofThrree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n == -1 || n == 0 || n == 2) {
+            return false;
+        }
+
+        if (n == 1) {
+            return true;
+        }
+
+        if (n % 3 != 0) {
+            return false;
+        }
+
+        return isPowerOfThree(n / 3);
+    }
+}
