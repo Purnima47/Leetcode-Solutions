ce478360de4ed186ddc87c6f2392f0872313f663
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-11-01 22:15:09 +0530

    Math, String, Greedy

--- 🟢 Easy/1903 - Largest Odd Number in String/_1903LargestOddNumberinString.java
+++ 🟢 Easy/1903 - Largest Odd Number in String/_1903LargestOddNumberinString.java
@@ -2,6 +2,7 @@ class Solution {
     public String largestOddNumber(String num) {
         int i = num.length() - 1;
         int to = 0;
+        
         while (i >= 0) {
             if ((num.charAt(i) - '0') % 2 != 0) {
                 to = i + 1;
--- 🟢 Easy/1903 - Largest Odd Number in String/_1903LargestOddNumberinString.js
+++ 🟢 Easy/1903 - Largest Odd Number in String/_1903LargestOddNumberinString.js
@@ -5,6 +5,7 @@
 var largestOddNumber = function (num) {
     let i = num.length - 1;
     let to = 0;
+    
     while (i >= 0) {
         if ((num.at(i) - '0') % 2 != 0) {
             to = i + 1;
