9b0ce0def3e0d5dbf5912a720a1d2f8e30ab5032
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-12-01 20:32:52 +0530

    Array, Two Pointers, DP, Stack, Monotonic Stack

--- 🔴 Hard/42 - Trapping Rain Water/_42TrappingRainWater.py
+++ 🔴 Hard/42 - Trapping Rain Water/_42TrappingRainWater.py
@@ -2,11 +2,14 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
 
+        # left max boundary --> auxilary array
         left = [0] * n
         left[0] = height[0]
         for i in range(1, n):
             left[i] = max(left[i - 1], height[i])
 
+
+        # right max boundary --> auxilary array
         right = [0] * n
         right[n - 1] = height[n - 1]
         for i in range(n - 2, -1, -1):
